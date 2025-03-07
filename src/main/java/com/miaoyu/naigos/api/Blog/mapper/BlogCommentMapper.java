6abdefb1f1b1d31025e1b40a0380674f3fe671d7
Yasenbaka <1260352143@qq.com>
2025-03-07 20:29:32 +0800

    修改blog文章评论的写入和输出逻辑

--- src/main/java/com/miaoyu/naigos/api/Blog/mapper/BlogCommentMapper.java
+++ src/main/java/com/miaoyu/naigos/api/Blog/mapper/BlogCommentMapper.java
@@ -11,12 +11,12 @@ import java.util.List;
 public interface BlogCommentMapper {
     @Select("SELECT * FROM api_blog_comment WHERE comment_id = #{comment_id}")
     BlogCommentEntity selectCommentById(@Param("comment_id") String comment_id);
-    @Select("SELECT * FROM api_blog_comment WHERE blog_id = #{blog_id}")
+    @Select("SELECT * FROM api_blog_comment WHERE blog_id = #{blog_id} ORDER BY upload_time DESC")
     List<BlogCommentEntity> selectCommentByBlogId(@Param("blog_id") String blog_id);
-    @Select("SELECT * FROM api_blog_comment WHERE author = #{author}")
+    @Select("SELECT * FROM api_blog_comment WHERE author = #{author} ORDER BY upload_time DESC")
     List<BlogCommentEntity> selectCommentByAuthor(@Param("author") String author);
 
-    @Insert("INSERT INTO api_blog_comment (comment_id, blog_id, author, content) VALUES (#{comment_id}, #{blog_id}, #{author}, #{content})")
+    @Insert("INSERT INTO api_blog_comment (comment_id, blog_id, author, content, upload_time) VALUES (#{comment_id}, #{blog_id}, #{author}, #{content}, #{upload_time})")
     boolean insertComment(BlogCommentEntity blogComment);
     @Delete("DELETE FROM api_blog_comment WHERE comment_id = #{comment_id}")
     boolean deleteCommentById(@Param("comment_id") String comment_id);
