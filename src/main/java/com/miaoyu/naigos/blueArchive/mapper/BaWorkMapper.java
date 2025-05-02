fdd27f611c2d1d71003805438463be7fade8360d
Yasenbaka <1260352143@qq.com>
2025-05-02 22:26:22 +0800

    新增获取BA单个作品的接口

--- src/main/java/com/miaoyu/naigos/blueArchive/mapper/BaWorkMapper.java
+++ src/main/java/com/miaoyu/naigos/blueArchive/mapper/BaWorkMapper.java
@@ -1,23 +1,24 @@
 package com.miaoyu.naigos.blueArchive.mapper;
 
 import com.miaoyu.naigos.blueArchive.model.BaWorkModel;
+import com.miaoyu.naigos.blueArchive.model.entity.BaWorkEntity;
 import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 public interface BaWorkMapper {
     @Select("SELECT * FROM ba_work")
-    List<BaWorkModel> selectAll();
+    List<BaWorkEntity> selectAll();
     @Select("SELECT * FROM ba_work WHERE first_classify = #{first_classify_id}")
-    List<BaWorkModel> selectListByFirstClassify(@Param("first_classify_id") String first_classify_id);
+    List<BaWorkEntity> selectListByFirstClassify(@Param("first_classify_id") String first_classify_id);
     @Select("SELECT bw.* FROM ba_work bw JOIN ba_student bst ON bw.student = bst.id JOIN ba_school bs ON bst.school = bs.id WHERE bs.id = #{school_id}")
-    List<BaWorkModel> selectListBySchoolId(@Param("school_id") String school_id);
+    List<BaWorkEntity> selectListBySchoolId(@Param("school_id") String school_id);
     @Select("SELECT bw.* FROM ba_work bw JOIN ba_student bst ON bw.student = bst.id JOIN ba_school_club bsc ON bst.club = bsc.id WHERE bsc.id = #{club_id}")
-    List<BaWorkModel> selectListByClubId(@Param("club_id") String club_id);
+    List<BaWorkEntity> selectListByClubId(@Param("club_id") String club_id);
     @Select("SELECT * FROM ba_work WHERE student = #{student_id}")
-    List<BaWorkModel> selectListByStudentId(@Param("student_id") String student_id);
+    List<BaWorkEntity> selectListByStudentId(@Param("student_id") String student_id);
     @Select("SELECT * FROM ba_work WHERE author = #{uuid}")
-    List<BaWorkModel> selectListByAuthor(@Param("uuid") String uuid);
+    List<BaWorkEntity> selectListByAuthor(@Param("uuid") String uuid);
     @Select("SELECT * FROM ba_work WHERE id = #{id}")
     BaWorkModel selectById(@Param("id") String id);
     @Insert("INSERT INTO ba_work (id, name, content, author, link, first_classify, student, cover_image, last_time) VALUES (#{id}, #{name}, #{content}, #{author}, #{link}, #{first_classify}, #{student}, #{cover_image}, #{last_time})")
--- src/main/java/com/miaoyu/naigos/blueArchive/service/BaWorkService.java
+++ src/main/java/com/miaoyu/naigos/blueArchive/service/BaWorkService.java
@@ -2,6 +2,7 @@ package com.miaoyu.naigos.blueArchive.service;
 
 import com.miaoyu.naigos.blueArchive.mapper.BaWorkMapper;
 import com.miaoyu.naigos.blueArchive.model.BaWorkModel;
+import com.miaoyu.naigos.constantsMap.ErrorMap;
 import com.miaoyu.naigos.constantsMap.SuccessMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,5 +24,14 @@ public class BaWorkService {
     /**根据club_id获取全部符合条件的work实体*/
     public Map<String, Object> getBaWorksByClubIdService(String clubId) {
         return new SuccessMap().standardSuccessMap(baWorkMapper.selectListByClubId(clubId));
+    }
+
+    /**根据work_id获取唯一符合条件的work实体*/
+    public Map<String, Object> getBaWorkByIdService(String workId) {
+        BaWorkModel work = baWorkMapper.selectById(workId);
+        if (Objects.isNull(work)) {
+            return new ErrorMap().resourceNotExist();
+        }
+        return new SuccessMap().standardSuccessMap(work);
     }
 }
